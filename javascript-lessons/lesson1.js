f51a1ad71528343ac905c3fb71e974a57dacb02e
Afza Khursheed <156500834+Afza-Khursheed123@users.noreply.github.com>
2025-01-28 10:53:12 +0000

    lesson1 uplaoded

---
+++ javascript-lessons/lesson1.js
@@ -0,0 +1,9 @@
+//creating common info table
+const AccountID = 231;
+let name = 'Abdullah Khursheed';
+let email= 'abdulla12345@gmail.com';
+let password = '12345';
+let city = 'Karachi'
+let phone_n0 = '03323145678';
+let accountstate ;//not defined
+console.table([AccountID,name,email,password,city,phone_n0,accountstate]);
