72fd5469a1026ef18bfe60cb90e0536fe468faef
Afza Khursheed <156500834+Afza-Khursheed123@users.noreply.github.com>
2025-01-28 11:12:21 +0000

    variables and its scope in js

--- javascript-lessons/lesson1.js
+++ javascript-lessons/lesson1.js
@@ -6,4 +6,13 @@ let password = '12345';
 let city = 'Karachi'
 let phone_n0 = '03323145678';
 let accountstate ;//not defined
-console.table([AccountID,name,email,password,city,phone_n0,accountstate]);
+//to display data in separate lines use log
+console.log(AccountID);
+console.log(name);
+console.log(email);
+console.log(password);
+console.log(city);
+console.log(phone_n0);
+console.log(accountstate);
+//to display data in table fromat
+console.table([AccountID,name,email,password,city,phone_n0,accountstate]); 
