ed58552e45f1bccd6a4f73c836980434d163fac1
Afza Khursheed <156500834+Afza-Khursheed123@users.noreply.github.com>
2025-01-28 12:15:09 +0000

    DataType conversion

---
+++ javascript-lessons/lesson3.js
@@ -0,0 +1,19 @@
+//converting string values to number
+let salary = '250000';
+//converting its datatype to number
+let salaryinnumber = Number(salary); 
+console.table([typeof(salary),typeof(salaryinnumber),salary,salaryinnumber]);
+
+
+//converting boolean values to number
+let ispaid = false;
+let ispaid_innumber = Number(ispaid); 
+console.table([typeof(ispaid),typeof(ispaid_innumber),ispaid,ispaid_innumber]);
+
+
+//converting number to string
+
+let score = 250000; 
+//converting its datatype to string
+let scoreinstring = String(score); 
+console.table([typeof(score),typeof(scoreinstring),score,scoreinstring]);
